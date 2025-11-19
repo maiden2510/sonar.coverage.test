cc81a676ca36c135e9c4294229757c0ec8499920
Johannes.Haas <johannes.haas@hoffmann-group.com>
2025-11-19 14:14:20 +0100

    Initialize `Sonar.Coverage.Service2` project with API setup, test project, and essential configuration files.

---
+++ Sonar.Coverage.Service2.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace Sonar.Coverage.Service2.Test;
+
+public class CalculatorTest
+{
+    [Test]
+    public void Test1()
+    {
+        Calculator.Decrement(10);
+        Calculator.Decrement(20);
+        Assert.Pass();
+    }
+}
---
+++ Sonar.Coverage.Service2/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace Sonar.Coverage.Service2;
+
+public class Calculator
+{
+    public static int Decrement(int value)
+    {
+        switch (value)
+        {
+            case 1: return 2;
+            case 2: return 3;
+            case 3: return 4;
+            case 4: return 5;
+            case 5: return 6;
+            default:throw new OverflowException(nameof(value));
+        }
+    }
+}
